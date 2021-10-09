27078aca16dc23295b3df1a3b66126f6e0c71497
Joseph Hellerstein <jlheller@uw.edu>
2021-10-09 10:55:32 -0700

    More initialization updates

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-# ** use sudo
 # Setup for a new raspbian system
 # Update installed software. Use "df -h" to see available space.
 # To see the version of buster "cat /etc/os-release"
+# Run using "bash setup.sh"
+#
+# Initialize the bashrc script
+mv $HOME/.bashrc /tmp
+cp bashrc $HOME/.bashrc
+# Get the BaseStack repository
 cd ..
+git clone https://github.com/ScienceStacks/BaseStack.git
+# Update software on the machine
 sudo apt update
 sudo apt full-upgrade
 sudo apt clean
 sudo apt get tightvncserver
-echo "Do sudo reboot to reboot"
 # Fix problem with numpy
 sudo apt-get install python-dev libatlas-base-dev
+#
+echo "Do sudo reboot to reboot"
